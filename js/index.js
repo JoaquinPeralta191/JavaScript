41698dfb5d6d1ec477289a58aa615f2571d2f4a1
Joaquin <Joaquin.Peralta.191@gmail.com>
2023-01-31 15:12:00 -0300

    actualizacion

--- js/index.js
+++ js/index.js
@@ -1,36 +1,87 @@
 //variables
 let busqueda = document.getElementById("buscador");
 let productosEnCarrito = []
+let sectionProductos = document.getElementById("productos")
+let modalBodyCarrito = document.getElementById("modal-bodyCarrito")
+let botonCarrito = document.getElementById("botonCarrito")
+let coincidencia = document.getElementById("coincidencia")
 //funciones
 
-function buscarPorNombre(arr){
-    let nombreBuscado = prompt("Ingrese el nombre del producto que desea buscar")
-    let nombreEncontrado = arr.filter(
-        (producto)=>producto.nombre.toLowerCase() == nombreBuscado.toLowerCase()
-        )
-    if(nombreEncontrado.length == 0){
-        console.log(`El producto ${nombreBuscado} no está en stock`)
-    }else{
-        mostrarCatalogoForEach(nombreEncontrado)
-    }    
- }
-function buscarProducto(busqueda, array){
- 
-    let busquedaArray = array.filter(
-    (producto) => producto.nombre.toLowerCase() == busqueda
-    
-)
+function mostrarCatalogo(array){
+    sectionProductos.innerHTML = ""
+    for(let producto of array){
+        let nuevoProductoDiv = document.createElement("div")
+        nuevoProductoDiv.classList.add("col-12", "col-md-6", "col-lg-4", "mb-3")
+        nuevoProductoDiv.innerHTML = `
+        <div id="${producto.id}" class="card tarjetas" style="width: 18rem;">
+                <img class="card-img-top img-fluid" style="height: 250px;"src="./../assets/images/img${producto.id}.jpg" alt="${producto.nombre}">
+                <div class="card-body">
+                    <h4 class="card-title">${producto.nombre}</h4>
+                    <p class="">Precio: ${producto.precio}</p>
+                <button id="agregarBtn${producto.id}" class="btn btn-outline-success">Agregar al carrito</button>
+                </div>
+        </div>
+        `
+        sectionProductos.appendChild(nuevoProductoDiv)
+        let agregarBtn = document.getElementById(`agregarBtn${producto.id}`)
+        agregarBtn.addEventListener("click", ()=>{
+            agregarAlCarrito(producto)
+        })
+} 
 }
+
+if(localStorage.getItem("carrito")){
+   productosEnCarrito = JSON.parse(localStorage.getItem("carrito"))
+   console.log(productosEnCarrito)
+}else{
+   productosEnCarrito = []
+   localStorage.setItem("carrito", productosEnCarrito)
+
+}
+
+function agregarAlCarrito(producto){
+
+   console.log(`El producto ${producto.nombre} ha sido agregado al carrito. Vale ${producto.precio}`)
+   productosEnCarrito.push(producto)
+   console.log(productosEnCarrito)
+   localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
+}
+
+function cargarProductosCarrito(array){
+    modalBodyCarrito.innerHTML = ""
+    array.forEach((productoEnCarrito)=>{
+        modalBodyCarrito.innerHTML +=
+        `
+        <div class="card border-primary mb-3" id ="productoCarrito${productoEnCarrito.id}" style="width: 100%;">
+                 <img class="card-img-top" height="300px" src="./../assets/images/img${productoEnCarrito.id}.jpg" alt="">
+                 <div class="card-body">
+                        <h4 class="card-title">${productoEnCarrito.nombre}</h4>
+                    
+                         <p class="card-text">$${productoEnCarrito.precio}</p> 
+                         <button class= "btn btn-danger" id="botonEliminar${productoEnCarrito.id}"><i class="fas fa-trash-alt"></i></button>
+                 </div>    
+            </div>
+        `
+    })
+}
+
+function buscarInfo(buscado, array){
+    let busquedaArray = array.filter(
+        (producto)=> producto.nombre.toLowerCase().includes(buscado)
+    )
+    busquedaArray.length == 0 ?
+    (coincidencia.innerHTML = `<h3>No hay coincidencias con su búsqueda</h3>`, mostrarCatalogo(busquedaArray)) 
+    :
+    (coincidencia.innerHTML = "", mostrarCatalogo(busquedaArray))
+    }
 //eventos
 
 busqueda.addEventListener("input", () => {
-    console.log(busqueda.value)
-    buscarProducto(busqueda.value, mostrador)
+    buscarInfo(busqueda.value.toLowerCase(), mostrador)
 })
 
-let btnCatalogo = document.getElementById("btnCatalogo")
+mostrarCatalogo(mostrador)
 
-btnCatalogo.onclick = () => {
-    mostrarCatalogo(mostrador)
-    sectionProductos.classList.add("espaciado")
-}
+botonCarrito.addEventListener("click", () => {
+    cargarProductosCarrito(productosEnCarrito)
+})
--- js/stock.js
+++ js/stock.js
@@ -14,66 +14,13 @@ class Producto {
  
  const producto2 = new Producto(2,"Chocotorta", 2100)
  
- const producto3 = new Producto(3,"Galletas", 1200)
+ const producto3 = new Producto(3,"Tarta de Frutilla", 1200)
  
  const producto4 = new Producto(4,"Alfajores", 2000)
  
  const producto5 = new Producto(5,"Tarta de limon", 1600)
  
- const producto6 = new Producto(6, "Budin glaseados", 1400)
+ const producto6 = new Producto(6, "Huevo de chocolate relleno", 1400)
  
  const mostrador = [producto1, producto2, producto3, producto4, producto5, producto6]
 
- let sectionProductos = document.getElementById("productos")
-
- function mostrarCatalogo(array){
-     //vaciar Div
-     sectionProductos.innerHTML = ""
-     //tenemos nuestros libros en estanteria:
-     for(let producto of array){
-         let nuevoProductoDiv = document.createElement("div")
-         //otra forma de sumarle una class a un elemento html
-         //classList + add agrego clases al elemento seleccionado
-         nuevoProductoDiv.classList.add("col-12", "col-md-6", "col-lg-4", "mb-3")
-         nuevoProductoDiv.innerHTML = `
-         <div id="${producto.id}" class="card tarjetas" style="width: 18rem;">
-                 <img class="card-img-top img-fluid" style="height: 200px;"src="./../assets/images/img${producto.id}.jpg" alt="${producto.nombre}">
-                 <div class="card-body">
-                     <h4 class="card-title">${producto.nombre}</h4>
-                     <p class="">Precio: ${producto.precio}</p>
-                 <button id="agregarBtn${producto.id}" class="btn btn-outline-success">Agregar al carrito</button>
-                 </div>
-         </div>
-         `
-         sectionProductos.appendChild(nuevoProductoDiv)
-         //captura agregarBtn
-         let agregarBtn = document.getElementById(`agregarBtn${producto.id}`)
-         //adjunto evento
-         agregarBtn.addEventListener("click", ()=>{
-             agregarAlCarrito(producto)
-         })
-} 
-}
-
-
-
-if(localStorage.getItem("carrito")){
-    //cuando ya existe algo en el storage entra aca:
-    productosEnCarrito = JSON.parse(localStorage.getItem("carrito"))
-    console.log(productosEnCarrito)
-}else{
-    productosEnCarrito = []
-    localStorage.setItem("carrito", productosEnCarrito)
-
-}
-//if para preguntar si entra por primera vez o si hay algo en storage
-
-
-function agregarAlCarrito(producto){
-
-    console.log(`El producto ${producto.nombre} ha sido agregado al carrito. Vale ${producto.precio}`)
-    //agregar libro al array de carrito
-    productosEnCarrito.push(producto)
-    console.log(productosEnCarrito)
-    localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
-}
