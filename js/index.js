1b3fbd45be91e42be6adf33d49eddfa29bd6720c
Joaquin <Joaquin.Peralta.191@gmail.com>
2023-02-09 21:50:07 -0300

    "actualizacion"

--- js/index.js
+++ js/index.js
@@ -5,7 +5,38 @@ let sectionProductos = document.getElementById("productos")
 let modalBodyCarrito = document.getElementById("modal-bodyCarrito")
 let botonCarrito = document.getElementById("botonCarrito")
 let coincidencia = document.getElementById("coincidencia")
+let botonEliminar = document.getElementById("botonEliminar")
+let precioTotal = document.getElementById("precioTotal")
+let precioParcial= document.getElementById("precioParcial")
 //funciones
+
+function agregarAlCarrito(producto){
+
+    let productoAgregado = productosEnCarrito.find((elem) => elem.id == producto.id)
+ 
+    if(productoAgregado == undefined){
+     console.log(`El producto ${producto.nombre} ha sido agregado al carrito. Vale ${producto.precio}`)
+    productosEnCarrito.push(producto)
+    console.log(productosEnCarrito)
+    localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
+    Toastify({
+     text: `Se ha agregado al carrito`,
+     duration: 3000,
+     gravity: "top",
+     position: "right",
+     style: {
+         background: "#7e066c",
+         color: "white"
+     }
+ }).showToast()
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Este producto ya se encuentra en el carrito',
+          })
+       }
+ }
 
 function mostrarCatalogo(array){
     sectionProductos.innerHTML = ""
@@ -39,31 +70,40 @@ if(localStorage.getItem("carrito")){
 
 }
 
-function agregarAlCarrito(producto){
-
-   console.log(`El producto ${producto.nombre} ha sido agregado al carrito. Vale ${producto.precio}`)
-   productosEnCarrito.push(producto)
-   console.log(productosEnCarrito)
-   localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
-}
-
 function cargarProductosCarrito(array){
     modalBodyCarrito.innerHTML = ""
     array.forEach((productoEnCarrito)=>{
         modalBodyCarrito.innerHTML +=
         `
-        <div class="card border-primary mb-3" id ="productoCarrito${productoEnCarrito.id}" style="width: 100%;">
-                 <img class="card-img-top" height="300px" src="./../assets/images/img${productoEnCarrito.id}.jpg" alt="">
+        <div class="card border-primary mb-3s" id ="productoCarrito${productoEnCarrito.id}" style="width: 100%;">
                  <div class="card-body">
                         <h4 class="card-title">${productoEnCarrito.nombre}</h4>
                     
                          <p class="card-text">$${productoEnCarrito.precio}</p> 
-                         <button class= "btn btn-danger" id="botonEliminar${productoEnCarrito.id}"><i class="fas fa-trash-alt"></i></button>
+                         <button class= "btn btn-danger botonEliminar" id="botonEliminar${productoEnCarrito.id}"><i class="fas fa-trash-alt"></i></button>
                  </div>    
             </div>
         `
     })
+
+    array.forEach((productoEnCarrito)=>{
+        document.getElementById(`botonEliminar${productoEnCarrito.id}`).addEventListener("click", () => {
+            let productoCont = document.getElementById(`productoCarrito${productoEnCarrito.id}`)
+            productoCont.remove()
+            let productoEliminar = array.find((producto) => producto.id == productoEnCarrito.id)
+            let posicion = array.indexOf(productoEliminar)
+            array.splice(posicion, 1)
+            localStorage.setItem("carrito", JSON.stringify(array))
+            calcularTotal(array)
+        })
+    })
+    calcularTotal(array)
 }
+
+function calcularTotal(array){
+    let total = array.reduce((acc, productoCarrito) => acc + productoCarrito.precio, 0)
+        total == 0 ? precioTotal.innerHTML = `No hay productos en el carrito` : precioTotal.innerHTML = `Total a pagar: ${total+total*0.20} `
+} 
 
 function buscarInfo(buscado, array){
     let busquedaArray = array.filter(
@@ -73,7 +113,11 @@ function buscarInfo(buscado, array){
     (coincidencia.innerHTML = `<h3>No hay coincidencias con su búsqueda</h3>`, mostrarCatalogo(busquedaArray)) 
     :
     (coincidencia.innerHTML = "", mostrarCatalogo(busquedaArray))
-    }
+}
+
+function eliminarDeCarrito(){
+    `botonEliminar${productoEnCarrito.id}`.innerHTML = ""
+}
 //eventos
 
 busqueda.addEventListener("input", () => {
@@ -81,6 +125,7 @@ busqueda.addEventListener("input", () => {
 })
 
 mostrarCatalogo(mostrador)
+
 
 botonCarrito.addEventListener("click", () => {
     cargarProductosCarrito(productosEnCarrito)
