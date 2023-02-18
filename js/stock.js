f64464c6c11499ee9655a069525b423508a4a2d4
Laura Torres <lauratorres@Lauras-MacBook-Pro.local>
2023-02-18 16:29:51 -0300

    A

--- js/stock.js
+++ js/stock.js
@@ -6,17 +6,17 @@ class Producto {
     }
 }
  
- const producto1 = new Producto(1,"Chesecake", 1900)
+//  const producto1 = new Producto(1,"Chesecake", 1900)
  
- const producto2 = new Producto(2,"Chocotorta", 2100)
+//  const producto2 = new Producto(2,"Chocotorta", 2100)
  
- const producto3 = new Producto(3,"Tarta de Frutilla", 1200)
+//  const producto3 = new Producto(3,"Tarta de Frutilla", 1200)
  
- const producto4 = new Producto(4,"Alfajores", 2000)
+//  const producto4 = new Producto(4,"Alfajores", 2000)
  
- const producto5 = new Producto(5,"Tarta de limon", 1600)
+//  const producto5 = new Producto(5,"Tarta de limon", 1600)
  
- const producto6 = new Producto(6, "Huevo de chocolate relleno", 1400)
+//  const producto6 = new Producto(6, "Huevo de chocolate relleno", 1400)
  
 
 let mostrador = []
@@ -35,9 +35,9 @@ const cargarMostrador = async () => {
 
 if(localStorage.getItem("mostrador")){
 
-    for(let libro of JSON.parse(localStorage.getItem("estanteria"))){
-        let libroNuevo = new Libro(libro.id, libro.autor, libro.titulo, libro.precio, libro.imagen)
-        estanteria.push(libroNuevo)
+    for(let producto of JSON.parse(localStorage.getItem("mostrador"))){
+        let productoNuevo = new Producto(producto.id, libro.nombre, producto.precio)
+        mostrador.push(productoNuevo)
     }
 }else{
     cargarMostrador()  
